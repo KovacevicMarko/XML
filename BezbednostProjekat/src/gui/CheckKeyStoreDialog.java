30271b33968f0b8600e05f6e9ef0dd9a15728e55
vladandesnica <vladan.d93@gmail.com>
2016-05-20 20:57:27 +0200

    Implementacija Dodavanja CA

--- BezbednostProjekat/src/gui/CheckKeyStoreDialog.java
+++ BezbednostProjekat/src/gui/CheckKeyStoreDialog.java
@@ -74,7 +74,7 @@ public class CheckKeyStoreDialog extends JDialog{
 					}
 				});
 				
-				okButton.addActionListener(new LoadKeyStoreAction(this, parentDialog, passwordField, fileName));
+				okButton.addActionListener(new LoadKeyStoreAction(this, parentDialog, passwordField, fileName,true));
 				
 				buttonPane.add(cancelButton);
 			}else{
@@ -91,7 +91,7 @@ public class CheckKeyStoreDialog extends JDialog{
 						that.dispose();
 					}
 				});
-				okButton.addActionListener(new LoadKeyStoreAction(this, parentDialog, passwordField, fileName));
+				okButton.addActionListener(new LoadKeyStoreAction(this, parentDialog, passwordField, fileName, false));
 				
 				buttonPane.add(cancelButton);
 			}
